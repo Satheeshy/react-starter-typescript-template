de0edb49c7d20b3ce6035e10895baa3fcf6a745f
satheesh <satheeshyadava@gmail.com>
2018-08-02 21:19:53 -0500

    starter-template-react-typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import * as React from 'react'
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                hello world
+            </div>
+        )
+    }
+}
+export default App;
